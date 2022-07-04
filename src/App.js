44b20414c6058e3bce0a53116108bbeea47a02a9
ostanmadalina92 <ostan.madalina@yahoo.com>
2022-07-04 15:42:25 +0300

    structure and styling added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
+import dice from "./images/icon-dice.svg";
+import dividerDesktop from "./images/pattern-divider-desktop.svg";
+import dividerMobile from "./images/pattern-divider-mobile.svg";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Advice #</h1>
-        <p> text </p>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.{" "}
+        </p>
         <picture>
-          <source
-            media="(min-width:768px)"
-            srcSet="./images/pattern-divider-desktop.svg"
-          />
-          <img src="./images/pattern-divider-mobile.svg" />
+          <source media="(min-width:768px)" srcSet={dividerDesktop}></source>
+          <img src={dividerMobile} />
         </picture>
+        <button className="dice-icon">
+          <img src={dice} />
+        </button>
       </div>
     </div>
   );
